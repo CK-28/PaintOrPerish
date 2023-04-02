695748aa331fe468f5f6623caec20f1f675e96fb
Alanna Tees <48448056+atees4836@users.noreply.github.com>
2023-04-01 21:39:53 -0400

    Added scoreboard to canvas. Add Updating score functions in TheGame script

--- Assets/Scripts/TheGame.cs
+++ Assets/Scripts/TheGame.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TheGame : MonoBehaviour
 {
+    public TMP_Text RedScoreText;
+    public TMP_Text BlueScoreText;
+    private int RedScore;
+    private int BlueScore;
+
     //bool gameStarted = true;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        updateRedScore(0);
+        updateBlueScore(0);
     }
 
     // Update is called once per frame
@@ -31,8 +38,17 @@ public class TheGame : MonoBehaviour
         {
             return false;
         }
+    }
 
-        //Debug.Log("Grabbing time in TheGame has failed");
-        //return false;
+    public void updateRedScore(int toAdd)
+    {
+        RedScore = RedScore + toAdd;
+        RedScoreText.text = "Red Score: " + RedScore;
+    }
+
+    public void updateBlueScore(int toAdd)
+    {
+        BlueScore = BlueScore + toAdd;
+        BlueScoreText.text = "Blue Score: " + BlueScore;
     }
 }
