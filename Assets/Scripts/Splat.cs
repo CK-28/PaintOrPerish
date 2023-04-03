d71f73f1676ec90ef51139221f2dbd7ecb2871fa
Lalfieri <78233106+Lalfieri@users.noreply.github.com>
2023-04-02 22:07:34 -0400

    Added some a form of paint splatter

--- Assets/Scripts/Splat.cs
+++ Assets/Scripts/Splat.cs
@@ -2,10 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Splat : MonoBehaviour
 {
+    public GameObject splatterMark;
+    private bool painted = false;
+    public void Update()
+    {
+        RaycastHit smt;
+        Vector3 forward = gameObject.transform.TransformDirection(Vector3.forward).normalized;
+        Debug.DrawRay(gameObject.transform.position, forward, Color.green);
+        if (Physics.Raycast(gameObject.transform.position, forward, out smt, 2f))
+        {
+            painted = true;
+            Instantiate(splatterMark, smt.point+(smt.normal *0.1f), Quaternion.FromToRotation(Vector3.forward, smt.normal));
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
+        if (!painted)
+        {
+            Instantiate(splatterMark, gameObject.transform.position, gameObject.transform.rotation);
+        }
+        //print("Splash!");
+
         Destroy(this.gameObject);
 
     }
