4d5ea3989c03b9c5ff13d5e2c379cbf61f7c108f
Crestena Khidhir <59154699+CK-28@users.noreply.github.com>
2023-06-30 13:47:34 -0400

    Adjusted comments for easier reading

--- Assets/Scripts/Controllers/StateMachine/StateObjective.cs
+++ Assets/Scripts/Controllers/StateMachine/StateObjective.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Playing the Objective state
 public class StateObjective : State
 {
 	public override void Execute(AIController character)
 	{
-		if (character.IsDead)											//If shot, die
+		if (character.IsDead)						//If shot, go to die
 		{
 			Debug.Log("Going from PlayingObjective to Dead");
 			character.ChangeState(new StateDie());
 		}
-		else if (character.EnemySeen() && character.EnemyInRange())     //If see and in range, attack
+		else if (character.EnemySeen() && character.EnemyInRange())     //If enemy is seen and in range, go to attack them
 		{
 			Debug.Log("Going from PlayingObjective to Attacking");
 			character.ChangeState(new StateShoot());
 		}
-		else if (character.EnemySeen() && !character.EnemyInRange())	//If see and out of range, approach
+		else if (character.EnemySeen() && !character.EnemyInRange())	//If enemy is seen and out of range, go to approach them
 		{
 			Debug.Log("Going from PlayingObjective to Approaching");
 			character.ChangeState(new StateApproach());
